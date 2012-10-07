7ca9da3e3a816f80769f60d16bb7463360efad8e
Brad Walker <bwalker8038@gmail.com>
2012-10-07 09:51:59 -0400

    fixed server initialization to work with socket.io

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ var app = express()
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
 
-
-  // Configuration
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -68,6 +66,11 @@ app.get('/threads/:id', thread.showThread);
 
 
 // Server
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
+});
+
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('hello');
 });
